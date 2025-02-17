c371288f1dd91f10453de870fb63b7ade18b2848
Trent11165 <ghost72839@gmail.com>
2025-02-17 04:07:11 -0500

    Update config.lua

--- wld-delallveh/config.lua
+++ wld-delallveh/config.lua
@@ -9,7 +9,7 @@ Config = {
     commandName = "dvall",
     -- This is the command that you will type into chat to execute the script.
 
-    restricCommand = true
+    restricCommand = false
     -- Setting this to false will allow anyone in the server to use the command. 
     -- If you set it to true you will need to add a ace perm to allow people to use it.
     -- Such as add_ace [GROUP] command.[commandName] allow
